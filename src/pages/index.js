a60c5c424b5383f7aa2bc737b8564cb4e5a2595c
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-05-27 14:47:45 -0700

    created homepage

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+const Home = () => {
+    return (
+        <>
+            <Head>
+                <title>Home</title>
+            </Head>
+            <div className='page-wrapper'>
+                <main>
+                    <h2>Here's your dumb homepage dummy</h2>
+                </main>
+            </div>
+        </>
+    );
+};
+
+export default Home;
