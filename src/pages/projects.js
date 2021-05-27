ee63e2652a9a7186146cd765202f27887f45c6cb
shame-wizards-apprentice <rachelnelsonschille@gmail.com>
2021-05-27 14:53:00 -0700

    created project page

---
+++ src/pages/projects.js
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+const Projects = () => {
+    return (
+        <>
+            <Head>
+                <title>Projects</title>
+            </Head>
+            <div className='page-wrapper'>
+                <main>
+                    <h2>Here's your dumb project page dummy</h2>
+                </main>
+            </div>
+        </>
+    )
+}
+
+export default Projects;
